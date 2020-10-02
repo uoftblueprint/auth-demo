7867a479eecb178561a3204e149225562519cf27
Olivia Li <oliviali.ouyang@gmail.com>
2020-10-01 19:03:37 -0700

    initial commit

---
+++ auth-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auth-demo
+//
+//  Created by Olivia Li on 2020-10-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
